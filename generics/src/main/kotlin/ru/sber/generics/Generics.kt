188193199af884c56a0ea2068ebaf06ccec38f11
Ivan Ilyin <geniusmov@yandex.ru>
2021-09-23 00:12:10 +0300

    add genericHomework

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,48 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
 import java.util.*
 
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <A, B> compare(p1: Pair<A, B>, p2: Pair<A, B>): Boolean {
+    return p1.equals(p2)
+}
 
 // 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <T : Comparable<T>> countGreaterThan(anArray: Array<T>, elem: T): Int {
+    var count: Int = 0
+
+    for (value in anArray) {
+        if (value > elem) {
+            count++
+        }
+    }
+
+    return count
 }
 
 // 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+class Sorter<T : Comparable<T>> {
+    val list: MutableList<T> = mutableListOf()
+
+    fun add(value: T) {
+        list.add(value)
+        list.sort();
+    }
 }
 
 // 4.
-class Stack {
+class Stack<T> {
+    private val stack: Deque<T> = ArrayDeque<T>()
 
+    fun push(value: T) {
+        stack.push(value)
+    }
+
+    fun pop(): T {
+        return stack.pop()
+    }
+
+    fun isEmpty(): Boolean {
+        return stack.isEmpty()
+    }
 }
